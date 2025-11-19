0d04bf6e6e6914dbada4f85f178141852041db44
NuclMe <saltansergei@gmail.com>
2025-11-19 22:39:42 +0200

    Update OAuth config to use env.js module

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const loginButton = document.querySelector('.login-button');
+import {
+  LINKEDIN_CLIENT_ID,
+  LINKEDIN_REDIRECT_URI,
+  LINKEDIN_SCOPE,
+} from './env.js';
 
-// TODO: Replace with your actual LinkedIn OAuth credentials
-const LINKEDIN_CLIENT_ID = 'YOUR_CLIENT_ID_HERE';
-const LINKEDIN_REDIRECT_URI = 'http://127.0.0.1:5500/';
-const LINKEDIN_SCOPE = 'YOUR_SCOPE_HERE';
+const loginButton = document.querySelector('.login-button');
 
 loginButton.addEventListener('click', () => {
   window.location.href = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${LINKEDIN_CLIENT_ID}&redirect_uri=${LINKEDIN_REDIRECT_URI}&scope=${LINKEDIN_SCOPE}`;
