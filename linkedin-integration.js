0f0d440c198892763d8ad6028edc4efdc29be2a1
bobanko <mishko.vladimir@gmail.com>
2025-11-29 08:21:58 +0200

    refac: fix env import, broke everything else

--- linkedin-integration.js
+++ linkedin-integration.js
@@ -1,11 +1,13 @@
 import {
   LINKEDIN_CLIENT_ID,
-  LINKEDIN_REDIRECT_URI,
+  // LINKEDIN_REDIRECT_URI,
   LINKEDIN_SCOPE,
-} from './env.js';
+} from "./.env.js";
 
-const loginButton = document.querySelector('.login-button');
+const loginButton = document.querySelector(".login-button");
 
-loginButton.addEventListener('click', () => {
+const LINKEDIN_REDIRECT_URI = document.location.href;
+
+loginButton.addEventListener("click", () => {
   window.location.href = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${LINKEDIN_CLIENT_ID}&redirect_uri=${LINKEDIN_REDIRECT_URI}&scope=${LINKEDIN_SCOPE}`;
 });
