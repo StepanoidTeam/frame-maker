12b747ac973bfee80444b98a3716c88f02647238
bobanko <mishko.vladimir@gmail.com>
2026-01-03 00:12:39 +0200

    refac: layout, drag-drop minor

--- drag-panels.js
+++ drag-panels.js
@@ -2,10 +2,6 @@
 /////     drag panels     /////
 ///////////////////////////////
 
-const body = document.querySelector("body");
-const board = document.getElementById('canvas-panel');
-
-let activatedMoving = true;
 let boardIsMoving = false;
 
 let initialDistance = 0;
@@ -13,32 +9,37 @@ let initialZoom = 100;
 let initialX = 0;
 let initialY = 0;
 
-body.addEventListener('mousemove', moveBoard);
-body.addEventListener('touchmove', moveBoard);
-
-board.addEventListener('mousedown', (e) => {
-  e.preventDefault();
-  if (activatedMoving) {
-    boardIsMoving = true;
-    initialX = e.clientX - parseFloat(getComputedStyle(board).getPropertyValue('left'));
-    initialY = e.clientY - parseFloat(getComputedStyle(board).getPropertyValue('top'));
-    board.classList.add("panning-active");
-  }
-});
-
-body.addEventListener('mouseup', (e) => {
-  e.preventDefault();
-  if (boardIsMoving) {
-    boardIsMoving = false;
-    board.classList.remove("panning-active");
-  }
-});
+document.body.addEventListener('mousemove', moveBoard);
+document.body.addEventListener('touchmove', moveBoard);
 
 function moveBoard(e) {
   if (boardIsMoving) {
     const newX = e.clientX - initialX;
     const newY = e.clientY - initialY;
-    board.style.setProperty('--x', `${newX}px`);
-    board.style.setProperty('--y', `${newY}px`);
+    $board.style.setProperty('--x', `${newX}px`);
+    $board.style.setProperty('--y', `${newY}px`);
   }
 }
+
+$board.addEventListener('mousedown', (e) => {
+  e.preventDefault();
+
+  boardIsMoving = true;
+  initialX =
+    e.clientX - parseFloat(getComputedStyle($board).getPropertyValue('left'));
+  initialY =
+    e.clientY - parseFloat(getComputedStyle($board).getPropertyValue('top'));
+  $board.classList.add('panning-active');
+
+  document.body.addEventListener(
+    'mouseup',
+    (e) => {
+      e.preventDefault();
+      if (boardIsMoving) {
+        boardIsMoving = false;
+        $board.classList.remove('panning-active');
+      }
+    },
+    { once: true }
+  );
+});
