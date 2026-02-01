9e6ca73f0882e1c390a7f15a1d56079ae6cc018f
NuclMe <saltansergei@gmail.com>
2026-02-01 21:19:51 +0200

    feat: added node env

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,16 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js';
-import { ENV } from './env.js';
 
+// Firebase configuration
+// Values are injected from .env file (local) or GitHub Secrets (deployment)
+// Run: npm run env:inject (for local development)
 const firebaseConfig = {
-  apiKey: ENV.FIREBASE_API_KEY,
-  authDomain: ENV.FIREBASE_AUTH_DOMAIN,
-  projectId: ENV.FIREBASE_PROJECT_ID,
-  storageBucket: ENV.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: ENV.FIREBASE_MESSAGING_SENDER_ID,
-  appId: ENV.FIREBASE_APP_ID,
-  measurementId: ENV.FIREBASE_MEASUREMENT_ID,
+  apiKey: '{{FIREBASE_API_KEY}}',
+  authDomain: '{{FIREBASE_AUTH_DOMAIN}}',
+  projectId: '{{FIREBASE_PROJECT_ID}}',
+  storageBucket: '{{FIREBASE_STORAGE_BUCKET}}',
+  messagingSenderId: '{{FIREBASE_MESSAGING_SENDER_ID}}',
+  appId: '{{FIREBASE_APP_ID}}',
+  measurementId: '{{FIREBASE_MEASUREMENT_ID}}',
 };
 
 const app = initializeApp(firebaseConfig);
